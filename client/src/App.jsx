69b02db24a9832aef1cade8d13f9b48752dd265c
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-14 13:40:22 +0200

    Initializing with MUI

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MyButton from './components/Button'
+import MenuAppBar from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <MenuAppBar />
+      <MyButton />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export default function MyButton() {
+  return (
+    <Button variant="contained">
+      Hello World
+    </Button>
+  );
+}
